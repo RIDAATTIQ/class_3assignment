fa54d13b698c783d6bb5de40c8326bd4ecc1c3b2
rida shiwani <167556213+RIDAATTIQ@users.noreply.github.com>
2024-10-22 21:37:06 +0100

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const heading = () => {
+  return (
+    <div>
+      <h1 className='bg-fuchsia-500 scroll-margin-top: 0.5rem font-extrabold font-style: italic  text-center text-teal-800 rounded-md border-solid px-2 py-2 mx-2 my-2 underline-offset-8 border-cyan-200 divide-emerald-200 rounded-lg '> HEADING </h1>
+      
+    
+    <h2 className='bg-pink-600 scroll-margin-bottom: 0.5rem font-SEMIbold font-style: italic  text-teal-400 drop-shadow-md	 '> THIS IS PARAGRAPH</h2>
+    <button className=' border-8 border-cyan-400 border-4  mx-2 my-2 ml-16'> LOGIN  </button>
+    <button className=' border-8 border-cyan-400 border-4  mx-2 my-2 ml-17'>LOGOUT </button>
+    
+    
+    </div>
+
+  )
+}
+
+export default heading
